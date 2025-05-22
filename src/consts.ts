631d0dba43d5d41b20458550a9d3316aad4fa2e2
ROBOT <tkata12@outlook.jp>
2025-05-23 00:40:36 +0900

    add: consts

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+export const REPOSITORY = {
+	url: "https://github.com/ROBOTofficial/website"
+};
+
+export const SITE_NAME = "WKTK.moe";
+export const SITE_DESCRIPTION =
+	"プログラマー向けの記事やブログを投稿していきます。空いた時間に気軽にご覧ください。";
