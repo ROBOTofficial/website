761d3eb9fc60f455bda18cbe46bd35d24a54e209
ROBOT <tkata12@outlook.jp>
2025-05-26 19:14:42 +0900

    fix: schema in content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,18 @@
-import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
-
-const extensions = ["md", "mdx", "webp", "png", "jpg"];
 
 export const collections = {
 	articles: defineCollection({
-		loader: glob({ base: "./src/content/articles", pattern: `**/*.{${extensions.join(",")}}` }),
 		schema: z.object({
 			title: z.string(),
 			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date()
+			updatedDate: z.coerce.date(),
+			url: z.string().optional(),
+			draft: z.boolean().optional().default(false),
+			tags: z.string().array().optional().default([]),
+			image: z.string().optional()
 		})
 	}),
 	specs: defineCollection({
-		loader: glob({ base: "./src/content/spec", pattern: `**/*.{${extensions.join(",")}}` }),
 		schema: z.object({
 			title: z.string(),
 			pubDate: z.coerce.date(),
