dd9a9cdbfb164d6778d183a576ecede30849845b
ROBOT <tkata12@outlook.jp>
2025-05-26 15:16:41 +0900

    add: sitemap and icon astro plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,25 @@ import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://wktk.moe",
 
-	integrations: [sitemap()],
+	integrations: [
+		icon({
+			include: {
+				"preprocess: vitePreprocess(),": ["*"],
+				"fa6-brands": ["*"],
+				"fa6-regular": ["*"],
+				"fa6-solid": ["*"]
+			}
+		}),
+		sitemap(),
+		mdx()
+	],
 
 	vite: {
 		plugins: [tailwindcss()]
