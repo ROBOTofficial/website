bd370a6d6e31aba297b07c07030bdc09a6714c2f
ROBOT <tkata12@outlook.jp>
2025-05-28 17:17:44 +0900

    add: profile constants val

--- src/consts.ts
+++ src/consts.ts
@@ -5,3 +5,54 @@ export const REPOSITORY = {
 export const SITE_NAME = "WKTK.moe";
 export const SITE_DESCRIPTION =
 	"プログラマー向けの記事やブログを投稿していきます。空いた時間に気軽にご覧ください。";
+
+export const PROFILE = {
+	name: "ROBOT",
+	description: "コードは気合で書けます。",
+	orgs: [
+		{
+			url: "https://thunlights.com",
+			title: "ThunLights",
+			icon: "/icon/thunlights.webp"
+		}
+	],
+	coding: [
+		{
+			url: "https://distopia.top",
+			title: "Distopia",
+			icon: "/icon/distopia.webp"
+		},
+		{
+			url: "https://verify.press",
+			title: "Verify.press",
+			icon: "/icon/verify.webp"
+		}
+	],
+	links: [
+		{
+			url: "https://github.com/ROBOTofficial",
+			title: "GITHUB",
+			icon: "/icon/github.webp"
+		},
+		{
+			url: "https://twitter.com/AlwaysHarapan",
+			title: "Twitter",
+			icon: "/icon/twitter.webp"
+		},
+		{
+			url: "https://www.npmjs.com/~robot_official",
+			title: "NPM",
+			icon: "/icon/npm.webp"
+		},
+		{
+			url: "https://zenn.dev/robot",
+			title: "Zenn",
+			icon: "/icon/zenn.webp"
+		},
+		{
+			url: "https://qiita.com/ROBOTofficial",
+			title: "Qiita",
+			icon: "/icon/qiita.webp"
+		}
+	]
+};
